68ecd83e767860d27eedadd97dd64eb25d91dd9d
gimaevra94 <gimaevra94@ya.ru>
2024-12-11 12:03:59 +0200

    checkpoint

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/olliefr/docker-gs-ping-roach
 
-go 1.16
+go 1.22
+
+toolchain go1.23.1
+
+require github.com/go-sql-driver/mysql v1.7.1
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,188 +1,38 @@
-/*func main() {
-
-	e := echo.New()
-
-		//Logger регистрирует HTTP-запросы.
-	e.Use(middleware.Logger())
-
-		//Recover восстанавливается после сбоев в любой точке цепочки
-		//и передает управление централизованному HTTPErrorHandler.
-	e.Use(middleware.Recover())
-
-	// инициирует бд
-	db, err := initStore()
-	if err != nil {
-		log.Fatalf("failed to initialise the store: %s", err)
-	}
-	defer db.Close()
-
-	e.GET("/", func(c echo.Context) error {
-		// отображает количество строк в таблице базы данных
-		return rootHandler(db, c)
-	})
-
-	e.GET("/ping", func(c echo.Context) error {
-		// просто показывает что статус ок. хз зачем
-		return c.JSON(http.StatusOK, struct{ Status string }{Status: "пошел нахуй"})
-	})
-
-	e.POST("/send", func(c echo.Context) error {
-		return sendHandler(db, c)
-	})
-
-	httpPort := os.Getenv("HTTP_PORT")
-	if httpPort == "" {
-		httpPort = "8000"
-	}
-
-	e.Logger.Fatal(e.Start(":" + httpPort))
-}
-
-type Message struct {
-	Value string `json:"value"`
-}
-
-func initStore() (*sql.DB, error) {
-	// инициирует бд
-
-	pgConnString := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable",
-		os.Getenv("PGHOST"),
-		os.Getenv("PGPORT"),
-		os.Getenv("PGDATABASE"),
-		os.Getenv("PGUSER"),
-		os.Getenv("PGPASSWORD"),
-	)
-
-	var (
-		db  *sql.DB
-		err error
-	)
-	openDB := func() error {
-		db, err = sql.Open("postgres", pgConnString)
-		return err
-	}
-
-	// повторение запросов в бд. хз что это. каких запросов?
-	err = backoff.Retry(openDB, backoff.NewExponentialBackOff())
-	if err != nil {
-		return nil, err
-	}
-
-	// что то типа приветствия когда впервые заходишь в бд через exec
-	if _, err := db.Exec(
-		"CREATE TABLE IF NOT EXISTS message (value TEXT PRIMARY KEY)"); err != nil {
-		return nil, err
-	}
-
-	return db, nil
-}
-
-func rootHandler(db *sql.DB, c echo.Context) error {
-	// отображает количество строк в таблице базы данных
-	r, err := countRecords(db)
-	if err != nil {
-		return c.HTML(http.StatusInternalServerError, err.Error())
-	}
-	return c.HTML(http.StatusOK, fmt.Sprintf("Hello, Docker! (%d)\n", r))
-}
-
-func sendHandler(db *sql.DB, c echo.Context) error {
-	// хуй знает нахуй это все надо но
-	// к запуску встроенного сервера
-	// это все равно отношения не имеет
-	m := &Message{}
-
-	if err := c.Bind(m); err != nil {
-		return c.JSON(http.StatusInternalServerError, err)
-	}
-
-	err := crdb.ExecuteTx(context.Background(), db, nil,
-		func(tx *sql.Tx) error {
-			_, err := tx.Exec(
-				"INSERT INTO message (value) VALUES ($1) ON CONFLICT (value) DO UPDATE SET value = excluded.value",
-				m.Value,
-			)
-			if err != nil {
-				return c.JSON(http.StatusInternalServerError, err)
-			}
-			return nil
-		})
-
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, err)
-	}
-
-	return c.JSON(http.StatusOK, m)
-}
-
-func countRecords(db *sql.DB) (int, error) {
-	//возвращает количество строк в таблице
-
-	rows, err := db.Query("SELECT COUNT(*) FROM message")
-	if err != nil {
-		return 0, err
-	}
-	defer rows.Close()
-
-	count := 0
-	for rows.Next() {
-		if err := rows.Scan(&count); err != nil {
-			return 0, err
-		}
-		rows.Close()
-	}
-
-	return count, nil
-}*/
-
-// main.go
-
-/*import (
-	"fmt"
-	"net/http"
-)
-
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
-	})
-
-	http.ListenAndServe(":8000", nil)
-}*/
-
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"net/http"
-)
+	"log"
+	"os"
 
-/*import (
-    "database/sql"
-    "fmt"
-    "log"
+	"github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-    // Настроить строку подключения
-    dsn := "root:root@tcp(127.0.0.1:3306)/mydb"
-    // Открыть подключение к базе данных
-    db, err := sql.Open("mysql", dsn)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer db.Close()
-    // Проверить подключение
-    if err := db.Ping(); err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println("Подключён к базе данных MySQL успешно!")
-}*/
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "хуй пизда, World!")
-	})
+	cfg := mysql.Config{
+		User:   os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net:    "tcp",
+		Addr:   "127.0.0.1:3306",
+		DBName: "db",
+	}
 
-	http.ListenAndServe(":8000", nil)
+	db, err := sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to the MySQL database successfully!")
 }
